bd18e9c7f70f0c65041d3871da517f8893637d00
Pedro Espíndula <joaopedro.jpse@gmail.com>
2018-07-08 20:43:19 -0300

    Adiciona padrão main e autenticação por json

--- analisador-de-sentimentos/twitter_data.py
+++ analisador-de-sentimentos/twitter_data.py
@@ -1,12 +1,40 @@
+# coding: utf-8
+
 import tweepy
+import json
 
-chave_api = ""
-senha_api = ""
 
-token = ""
-token_senha = ""
+def trending_topics(api):
+    pass
 
-autenticacao = tweepy.OAuthHandler(chave_api, senha_api)
-autenticacao.set_acess_token(token, token_senha)
 
-api = tweepy.API(autenticacao)
+def tweets_palavra(api, palavra_chave):
+    "Procura tweets sobre uma palavra_chave"
+    tweets = []
+    for tweet in api.search(palavra_chave):
+        tweets.append(tweet.text)
+
+    return tweets
+
+
+def main():
+    """Provem interação com a API do twitter"""
+    with open('auth.json', 'r') as a:
+        dic_aut = json.load(a)
+
+    autenticacao = tweepy.OAuthHandler(dic_aut['chave_api'], dic_aut['senha_api'])
+    autenticacao.set_access_token(dic_aut['token'], dic_aut['token_senha'])
+
+    api = tweepy.API(autenticacao)
+
+    palavra_chave = raw_input('Digite a palavra-chave: ')
+    tweets = tweets_palavra(api, palavra_chave)
+
+    print tweets
+
+    return tweets
+
+
+if __name__ == "__main__":
+    main()
+
