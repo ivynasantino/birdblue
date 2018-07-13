ab0fa4d2576525129a87cec9b20d13a6b2ff5936
IvynaSantino <ivyna.alves@ccc.ufcg.edu.br>
2018-07-13 08:05:49 -0300

    refatora arquivo

--- analisador-de-sentimentos/twitter_data.py
+++ analisador-de-sentimentos/twitter_data.py
@@ -3,7 +3,6 @@
 import tweepy
 import json
 
-
 # API.trends_available()
 def tweets_tts(api):
     "Procura tweets baseados nos TTs"
@@ -20,22 +19,23 @@ def tweets_palavra(api, palavra_chave):
     return tweets
 
 
+def autenticacao(dic_aut):
+	autenticacao = tweepy.OAuthHandler(dic_aut['chave_api'], dic_aut['senha_api'])
+	autenticacao.set_access_token(dic_aut['token'], dic_aut['token_senha'])
+
+	return autenticacao
+	
 def main():
     """Provem interação com a API do twitter"""
     with open('auth.json', 'r') as a:
         dic_aut = json.load(a)
 
-    autenticacao = tweepy.OAuthHandler(dic_aut['chave_api'], dic_aut['senha_api'])
-    autenticacao.set_access_token(dic_aut['token'], dic_aut['token_senha'])
-
-    api = tweepy.API(autenticacao)
+    api = tweepy.API(autenticacao(dic_aut))
 
     palavra_chave = raw_input('Digite a palavra-chave: ')
     tweets = tweets_palavra(api, palavra_chave)
 
     print tweets
-
-    return tweets
 
 
 if __name__ == "__main__":
