efd6b23621c0479319020a34c848ded470f119c3
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-08-09 18:02:26 -0300

    adiciona script para gerar o csv e adicionar dados

--- analisador_de_sentimentos/script_geracsv.py
+++ analisador_de_sentimentos/script_geracsv.py
@@ -1,5 +1,30 @@
 import csv
 
-data = csv.reader(open(""), r)
+def gera_coluna_id(nome_csv):
+	linha_id = ['id_tt', 
+				'sentimento', 
+				'id_usuario', 
+				'texto', 
+				'data', 
+				'retweet', 
+				'retweet_num',
+				'idioma']
 
-
+	with open (nome_csv, 'a') as data:
+		return csv.writer(data).writerow(linha_id)
+		
+def adicionaDados(tweets_dados, nome_csv):
+	
+	for (tt, tt_sentiment) in tweets_dados:
+		inserir = [tt.id, 
+				   tt_sentiment, 
+				   tt.user.id, 
+				   tt.text,
+				   tt.created_at,
+				   tt.retweeted,
+				   tt.retweet_count,
+				   tt.lang]
+	
+		with open (nome_csv, 'a') as data:
+			writer = csv.writer(data)
+			writer.writerows(inserir)
