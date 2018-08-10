5c4e6b8ea54fa021fe8154b0ab229f6aebca3d25
Pedro Espíndula <joaopedro.jpse@gmail.com>
2018-08-10 15:09:57 -0300

    Adiciona analise de textos traduzidos

--- analisador_de_sentimentos/twitter_data.py
+++ analisador_de_sentimentos/twitter_data.py
@@ -1,8 +1,5 @@
 # coding: utf-8
 
-
-from __future__ import print_function, unicode_literals
-
 from textblob import TextBlob
 import tweepy
 import json
@@ -17,11 +14,21 @@ def tweets_tts(api):
 # API.search(kw[, lang][, locale][, rpp][, page][, since_id][, geocode][, show_user])
 def tweets_palavra(api, palavra_chave):
     "Procura tweets sobre uma palavra_chave"
+
     tweets = []
+
     for tweet in api.search(palavra_chave):
-        sentiment = TextBlob(tweet.text).sentiment.polarity
-        tupla_tweet = (tweet, sentiment)
-        tweets.append(tupla_tweet)
+        try:
+            frase = TextBlob(tweet.text)
+            if frase.detect_language() != 'en':
+                frase = TextBlob(str(frase.translate(to='en')))
+
+            sentiment = frase.sentiment.polarity
+            tupla_tweet = (tweet, sentiment)
+            tweets.append(tupla_tweet)
+        except UnicodeDecodeError:
+            pass
+
 
     return tweets
 
