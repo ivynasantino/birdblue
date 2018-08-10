f48aab0437c08580cabeb705261b51bc3994ed62
Pedro Espíndula <joaopedro.jpse@gmail.com>
2018-08-10 14:20:19 -0300

    Refatora script_geracsv

--- analisador_de_sentimentos/script_geracsv.py
+++ analisador_de_sentimentos/script_geracsv.py
@@ -1,30 +1,29 @@
+from __future__ import unicode_literals
+
+import os
 import csv
 
-def gera_coluna_id(nome_csv):
-	linha_id = ['id_tt', 
-				'sentimento', 
-				'id_usuario', 
-				'texto', 
-				'data', 
-				'retweet', 
-				'retweet_num',
-				'idioma']
+def adicionaDados(palavra_chave, dados, cabecalho):
+    path_dir = os.path.join(os.path.expanduser('~/'), 'csv_analise')
 
-	with open (nome_csv, 'a') as data:
-		return csv.writer(data).writerow(linha_id)
-		
-def adicionaDados(tweets_dados, nome_csv):
-	
-	for (tt, tt_sentiment) in tweets_dados:
-		inserir = [tt.id, 
-				   tt_sentiment, 
-				   tt.user.id, 
-				   tt.text,
-				   tt.created_at,
-				   tt.retweeted,
-				   tt.retweet_count,
-				   tt.lang]
-	
-		with open (nome_csv, 'a') as data:
-			writer = csv.writer(data)
-			writer.writerows(inserir)
+    if not os.path.isdir(path_dir):
+        os.mkdir(path_dir)
+
+    path_csv = os.path.join(path_dir, palavra_chave + '.csv')
+
+    with open(path_csv, 'a') as f:
+
+        writer = csv.writer(f)
+        writer.writerow(cabecalho)
+
+        for (tt, sentimento) in dados:
+            dados_inseridos = [tt.id,
+                               sentimento,
+                               tt.user.id,
+                               tt.text.encode('utf-8'),
+                               tt.created_at,
+                               tt.retweeted,
+                               tt.retweet_count,
+                               tt.lang]
+            writer.writerow(dados_inseridos)
+
